c56a18fc4059ed9304eb30868f84acd33a008bae
vitosandrin <sandrinvito10@gmail.com>
2024-03-04 23:17:26 -0300

    Adicionar arquivos de projeto.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcServer;
+using static GrpcServer.Greeter;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7278");
+
+GreeterClient client = new(channel);
+
+HelloRequest inputRequest = new() { Name = "GreeterClient" };
+
+HelloReply reply = await client.SayHelloAsync(inputRequest);
+
+Console.WriteLine(reply.Message);
+
+Console.ReadLine();
