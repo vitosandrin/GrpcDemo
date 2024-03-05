d729ea04374ba17e5a8a2aa9152eb84a6a2be5f2
vitosandrin <sandrinvito10@gmail.com>
2024-03-04 23:36:34 -0300

    working

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,16 +2,24 @@
 
 using Grpc.Net.Client;
 using GrpcServer;
+using static GrpcServer.Customer;
 using static GrpcServer.Greeter;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7278");
 
+CustomerClient customerClient = new(channel);
 GreeterClient client = new(channel);
+
+CustomerLookupModel customerLookup = new() { UserId = 2 };
+
+CustomerModel customer = customerClient.GetCustomerInfo(customerLookup);
 
 HelloRequest inputRequest = new() { Name = "GreeterClient" };
 
 HelloReply reply = await client.SayHelloAsync(inputRequest);
 
+
 Console.WriteLine(reply.Message);
+Console.WriteLine(customer.Name);
 
 Console.ReadLine();
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new();
+
+            if (request.UserId == 1)
+            {
+                output.Name = "John";
+            }
+            else if (request.UserId == 2)
+            {
+                output.Name = "Jane";
+            }
+            else
+            {
+                output.Name = "No User";
+            }
+
+            return Task.FromResult(output);
+        }
+
+    }
+}
